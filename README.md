0c53c8362a51008853c629e5abe6ef62c41d9324
Alex H <alex.holder@bigfootds.com>
2023-06-01 21:00:24 +1000

    feat!: add middleware and error handling

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Lesson repo for 1st of June 2023.
 	- built-in middleware 
 	- error-handling middleware
 	- validation via middleware
+
+
+Routes must either:
+- throw an error or pass an error to next() to jump to the error-handling middleware
+- must call next() to move into the next step of middleware chain
+- respond with response.send() or response.json()
--- src/server.js
+++ src/server.js
@@ -3,11 +3,103 @@ const app = express();
 
 //const app = require('express')();
 
-app.get("/", (request, response) => {
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use((request, response, next) => {
+	console.log("middleware happened!");
+
+	request.info = {
+		url: request.originalUrl,
+		ip: request.ip,
+		route: request.route
+	};
+
+	// //Error style 1
+	// if (request.info.weather == null){
+	// 	throw("no weather provided");
+	// }
+
+
+
+	next();
+});
+
+
+app.use((error, request, response, next) => {
+	console.log("Some error occured!");
+
+	// response.json({
+	// 	error: error,
+	// 	message: "Server doesn't like you",
+	// 	favouriteColour:"blue"
+	// })
+
+	next();
+
+});
+
+
+
+app.get("/", (request, response, next) => {
+
+	if (request.info.weather == null){
+
+		// Style 1 on how to trigger error handling:
+		//throw("Missing weather information");
+
+		// Style 2 on how to trigger error handling:
+		let someError = new Error("Missing weather information");
+		someError.status = 401;
+		next(someError);
+	}
+
 	response.json({
-		message:"Hello world"
+		message:"Hello world",
+		info: request.info
 	})
 });
+
+
+
+
+
+app.post("/", (request, response) => {
+
+	if (request.info.weather == null){
+		response.json({
+			error:"Error occured"
+		})
+	}
+
+	response.json({
+		receivedData: request.body
+	});
+});
+
+
+
+
+
+
+
+
+
+app.use("/dynamicRoute/", (request, response, next) => {
+	console.log("middleware happened but not for the root route!");
+
+	next(new Error("Error for the lols"));
+});
+
+app.get("/someNewRoute", (request, response) => {
+	response.json({
+		message:"Hello there",
+		info: request.info
+	});
+});
+
 
 app.get("/dynamicRoute/:message/:favouriteColour", (request, response) => {
 	let message = request.params.message;
@@ -20,9 +112,26 @@ app.get("/dynamicRoute/:message/:favouriteColour", (request, response) => {
 		// 	params: params
 		// }
 		params: request.params,
-		queryParams: request.query
+		queryParams: request.query,
+		info: request.info
 	})
 })
+
+
+
+app.use((error, request, response, next) => {
+	console.log("second error handling middleware runs")
+	response.json({
+		message:"some error",
+		status: error.status,
+		errorMessage: error.message
+	})
+});
+
+
+// some hypothetical controller stuff here
+// no access to middleware because it was imported _after_
+// all middleware 
 
 
 module.exports = {
